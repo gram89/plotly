0a0cdef6fbfaedd64466bfc6221e5a2a75a3cb8b
gram89 <89284806+gram89@users.noreply.github.com>
2021-11-21 20:33:39 -0500

    Asynchronous

--- Asynchronous/BBB/bbb.js
+++ Asynchronous/BBB/bbb.js
@@ -29,3 +29,14 @@ d3.json("samples.json").then(function(data){
     Object.entries(firstPerson).forEach(([key,value]) => { console.log(key +": "+ value)});
 });
 
+d3.selectAll("body").on("change",updatePage);
+
+function updatePage() {
+    let dropdownMenu = d3.selectAll("#selectOption").node();
+    let dropdownMenuId = dropdownMenu.id;
+    let selectedOption = dropdownMenu.value;
+
+    console.log(dropdownMenuId);
+    console.log(selectedOption);
+    console.log(d3.selectAll("#menu").node().id);
+};
